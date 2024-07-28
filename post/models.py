e0350170ab701153b4f43884f5312835a467f47d
soheiljavadi <javadi.soheil@yahoo.com>
2024-07-28 16:16:35 +0330

    update model

--- post/models.py
+++ post/models.py
@@ -9,8 +9,27 @@ class Blog(models.Model):
        content = models.TextField(default='hi')
        created_at = models.DateTimeField(auto_now_add=True,null=True)
        updated_at = models.DateTimeField(auto_now=True,null=True)
+       pic = models.ImageField('Image',
+                            upload_to='Post_image',
+                            help_text='picture for post',)
        
 
        def __str__(self):
         return self.content[:20]
+       
+class Like(models.Model):
+     Blog = models.ForeignKey(Blog, related_name='likes', on_delete=models.CASCADE)
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
+        
+class Comment(models.Model):
+    Blog = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    content = models.TextField(default='hello')
+   
+    def __str__(self):
+        return  {self.user.first_name} 
+
+
+
+     
